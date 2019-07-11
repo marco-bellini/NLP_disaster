c23cf9788ed5564221c5b1eab94bc5563e1faf0d
marco-bellini <marco.bellini.it@gmail.com>
2019-07-11 13:33:16 +0200

    fix

--- nlp_spacy.py
+++ nlp_spacy.py
@@ -6,6 +6,7 @@ import spacy
 from spacy.tokenizer import Tokenizer
 from spacy.lang.en import English
 from spacy.util import minibatch, compounding
+import random as rd
 
 import plac
 import re
@@ -14,13 +15,109 @@ from pathlib import Path
 
 
 @plac.annotations(
-    download_files=("Download data files True / False", "flag", "d" ),
-    model=("Model name. Defaults to blank 'en' model.", "option", "m", str),
+    download =("Download data files", "flag", "d", bool ),
+    model=("Model name. Defaults to blank en_core_web_md model.", "option", "m", str),
     output_dir=("Optional output directory", "option", "o", Path),
     n_texts=("Number of texts to train from", "option", "t", int),
     n_iter=("Number of training iterations", "option", "n", int),
+    column=("data column to fit default: food", "option", "c", str)   
 )
 
+def main( download , model=None, output_dir=None, n_iter=20, n_texts=2000, column='food'):
+    
+    #print(download , model, output_dir, n_iter, n_texts)
+
+    if download:
+        print("downloading files...")
+        download_data_files()
+        print("done")
+        print()
+
+    
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        if not output_dir.exists():
+            output_dir.mkdir()
+
+    print("Loading model...")
+    if model is not None:
+        nlp = spacy.load(model)  # load existing spaCy model
+        print("Loaded model '%s'" % model)
+    else:
+        nlp = spacy.load("en_core_web_md")
+        print("Loaded model '%s'" % "en_core_web_md")
+    print()
+    
+    df=pd.read_csv('disaster_response_messages_training.csv')
+    df=df.drop(columns=['original','split'])
+    train =pd.concat((df['message'], df[column]),axis=1).values
+
+    # initialized cnn
+    if "textcat" not in nlp.pipe_names:
+        textcat = nlp.create_pipe(
+            "textcat",
+            config={
+                "exclusive_classes": True,
+                "architecture": "simple_cnn",
+            }
+        )
+        nlp.add_pipe(textcat, last=True)
+    # otherwise, get it, so we can add labels to it
+    else:
+        textcat = nlp.get_pipe("textcat")
+    
+    (train_texts, train_cats), (dev_texts, dev_cats) = load_data(train)
+    
+    textcat.add_label('POSITIVE')
+    textcat.add_label("NEGATIVE")
+
+    train_texts = train_texts[:n_texts]
+    train_cats = train_cats[:n_texts]
+
+    print()
+    print(
+            "Using {} examples ({} training, {} evaluation)".format(
+                n_texts, len(train_texts), len(dev_texts)
+            )
+        )
+    
+    train_data = list(zip(train_texts, [{"cats": cats} for cats in train_cats]))
+
+    # get names of other pipes to disable them during training
+    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "textcat"]
+    
+    init_tok2vec=None
+
+    with nlp.disable_pipes(*other_pipes):  # only train textcat
+        optimizer = nlp.begin_training()
+
+        print("Training the model...")
+        print("{:^5}\t{:^5}\t{:^5}\t{:^5}".format("LOSS", "P", "R", "F"))
+        batch_sizes = compounding(4.0, 32.0, 1.001)
+        for i in range(n_iter):
+            losses = {}
+            # batch up the examples using spaCy's minibatch
+            rd.shuffle(train_data)
+            batches = minibatch(train_data, size=batch_sizes)
+            for batch in batches:
+                texts, annotations = zip(*batch)
+                nlp.update(texts, annotations, sgd=optimizer, drop=0.2, losses=losses)
+            with textcat.model.use_params(optimizer.averages):
+                # evaluate on the dev data split off in load_data()
+                scores = evaluate(nlp.tokenizer, textcat, dev_texts, dev_cats)
+            print(
+                "{0:.3f}\t{1:.3f}\t{2:.3f}\t{3:.3f}".format(  # print a simple table
+                    losses["textcat"],
+                    scores["textcat_p"],
+                    scores["textcat_r"],
+                    scores["textcat_f"],
+                )
+            )
+
+    with nlp.use_params(optimizer.averages):
+        nlp.to_disk(output_dir)
+    print("Saved model to", output_dir)
+    
 
 def download_data_files():
     # download the dataset 
@@ -73,7 +170,7 @@ def load_data(train_data,limit=0, split=0.8):
     """Load data from the IMDB dataset."""
     # Partition off part of the train data for evaluation
 
-    random.shuffle(train_data)
+    rd.shuffle(train_data)
     train_data = train_data[-limit:]
     texts, labels = zip(*train_data)
     cats = [{"POSITIVE": bool(y), "NEGATIVE": not bool(y)} for y in labels]
@@ -111,93 +208,7 @@ def evaluate(tokenizer, textcat, texts, cats):
     return {"textcat_p": precision, "textcat_r": recall, "textcat_f": f_score}
 
 
-def main( download_files , model=None, output_dir=None, n_iter=20, n_texts=2000):
-    
-    print(download_files, model, output_dir, n_iter, n_texts)
-    
-    if output_dir is not None:
-        output_dir = Path(output_dir)
-        if not output_dir.exists():
-            output_dir.mkdir()
 
-    if model is not None:
-        nlp = spacy.load(model)  # load existing spaCy model
-        print("Loaded model '%s'" % model)
-    else:
-        nlp = spacy.load("en_core_web_md")
-        print("Loaded model '%s'" % "en_core_web_md")
-
-    if download_files:
-        print("downloading files...")
-        download_data_files()
-    
-    df=pd.read_csv('disaster_response_messages_training.csv')
-    df=df.drop(columns=['original','split'])
-    train =pd.concat((df['message'], df[column]),axis=1).values
-
-    # initialized cnn
-    if "textcat" not in nlp.pipe_names:
-        textcat = nlp.create_pipe(
-            "textcat",
-            config={
-                "exclusive_classes": True,
-                "architecture": "simple_cnn",
-            }
-        )
-        nlp.add_pipe(textcat, last=True)
-    # otherwise, get it, so we can add labels to it
-    else:
-        textcat = nlp.get_pipe("textcat")
-    
-    textcat.add_label('POSITIVE')
-    textcat.add_label("NEGATIVE")
-
-    train_texts = train_texts[:n_texts]
-    train_cats = train_cats[:n_texts]
-
-    print(
-            "Using {} examples ({} training, {} evaluation)".format(
-                n_texts, len(train_texts), len(dev_texts)
-            )
-        )
-    
-    train_data = list(zip(train_texts, [{"cats": cats} for cats in train_cats]))
-
-    # get names of other pipes to disable them during training
-    other_pipes = [pipe for pipe in nlp.pipe_names if pipe != "textcat"]
-    
-    init_tok2vec=None
-
-    with nlp.disable_pipes(*other_pipes):  # only train textcat
-        optimizer = nlp.begin_training()
-
-        print("Training the model...")
-        print("{:^5}\t{:^5}\t{:^5}\t{:^5}".format("LOSS", "P", "R", "F"))
-        batch_sizes = compounding(4.0, 32.0, 1.001)
-        for i in range(n_iter):
-            losses = {}
-            # batch up the examples using spaCy's minibatch
-            random.shuffle(train_data)
-            batches = minibatch(train_data, size=batch_sizes)
-            for batch in batches:
-                texts, annotations = zip(*batch)
-                nlp.update(texts, annotations, sgd=optimizer, drop=0.2, losses=losses)
-            with textcat.model.use_params(optimizer.averages):
-                # evaluate on the dev data split off in load_data()
-                scores = evaluate(nlp.tokenizer, textcat, dev_texts, dev_cats)
-            print(
-                "{0:.3f}\t{1:.3f}\t{2:.3f}\t{3:.3f}".format(  # print a simple table
-                    losses["textcat"],
-                    scores["textcat_p"],
-                    scores["textcat_r"],
-                    scores["textcat_f"],
-                )
-            )
-
-    with nlp.use_params(optimizer.averages):
-        nlp.to_disk(output_dir)
-    print("Saved model to", output_dir)
-    
 
 if __name__ == "__main__":
     plac.call(main)
